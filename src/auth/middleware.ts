f3a23a13f97e42c83d5e9a60952335a364a93aeb
Rishik <rishik3555@gmail.com>
2025-04-26 00:56:26 +0530

    added middleware

---
+++ src/auth/middleware.ts
@@ -0,0 +1,22 @@
+import jwt from "jsonwebtoken";
+
+export default function authenticateAdmin(req: { header: (arg0: string) => any; admin: any; }, res: { status: (arg0: number) => { (): any; new(): any; json: { (arg0: { message: string; }): void; new(): any; }; }; }, next: () => void) {
+  const token = req.header("Authorization");
+
+  if (!token) {
+    return res
+      .status(401)
+      .json({ message: "Access denied. No token provided." });
+  }
+
+  try {
+
+    const verified = jwt.verify(token.split(" ")[1], process.env.JWT_SECRET);
+
+    req.admin = verified;
+    next();
+  } catch (error) {
+    console.error("JWT Verification Error:", error);
+    res.status(400).json({ message: "Invalid token" });
+  }
+}
